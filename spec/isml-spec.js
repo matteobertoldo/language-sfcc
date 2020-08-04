0c99d551a6276357b2dd221930833c1a90ddd69e
Matteo Bertoldo <mrbert93@gmail.com>
2020-08-04 18:57:14 +0200

    feat(datatips): isml custom provider

--- spec/isml-spec.js
+++ spec/isml-spec.js
@@ -1,17 +1,17 @@
 'use babel';
 
 describe('Language SFCC', () => {
-    let grammar = null;
+  let grammar = null;
 
-    beforeEach(() => {
-        waitsForPromise(() => atom.packages.activatePackage('language-sfcc'));
-        runs(() => (grammar = atom.grammars.grammarForScopeName('text.html.isml')));
-    });
+  beforeEach(() => {
+    waitsForPromise(() => atom.packages.activatePackage('language-sfcc'));
+    runs(() => (grammar = atom.grammars.grammarForScopeName('text.html.isml')));
+  });
 
-    describe('when Atom run language process', () => {
-        it('parses the ISML grammar', () => {
-            expect(grammar).toBeTruthy();
-            return expect(grammar.scopeName).toBe('text.html.isml');
-        });
+  describe('when Atom run language process', () => {
+    it('parses the ISML grammar', () => {
+      expect(grammar).toBeTruthy();
+      return expect(grammar.scopeName).toBe('text.html.isml');
     });
+  });
 });
